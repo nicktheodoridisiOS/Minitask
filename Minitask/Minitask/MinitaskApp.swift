e9568a640377b9929bd73d9b62dc7fc01af8b2c1
Nick Theodoridis <122683142+nicktheodoridisiOS@users.noreply.github.com>
2023-03-05 23:33:35 +0200

    Create project

---
+++ Minitask/Minitask/MinitaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinitaskApp.swift
+//  Minitask
+//
+//  Created by Nick Theodoridis on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MinitaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
